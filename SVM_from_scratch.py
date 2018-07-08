905dfca11848ada8a660ef352d01500a094aed39
Tushar <kansaltushar18@gmail.com>
2018-07-08 12:32:22 +0530

    Added implementation of fit method

--- SVM_from_scratch.py
+++ SVM_from_scratch.py
@@ -19,8 +19,55 @@ class Support_Vector_Machine:
     
     # train
     def fit(self, data):
-        pass
+        self.data = data
+        # {||w||: [w,b]} Selecting the one with min. value of w
+        opt_dict = {}
+        
+        transform = [[1,1],
+                     [-1,1],
+                     [-1,-1],
+                     [1,-1]]
+        
+        all_data = []
+        
+        for yi in self.data:
+            for featureset in self.data[yi]:
+                for feature in featureset:
+                    all_data.append(feature)
+        
+        self.max_feature_value = max(all_data)
+        self.min_feature_value = min(all_data)
+        all_data = None
+        
+        
+        step_sizes = [self.max_feature_value * 0.1,
+                      self.max_feature_value * 0.01,
+                      self.max_feature_value * 0.001]
+        
+        b_range_multiple = 5
+        
+        b_multiple = 5
+        latest_optimum = self.max_feature_value * 10
+        
+        for step in step_sizes:
+            w = np.array([latest_optimum, latest_optimum])
+            
+            optimized = False
+            while not optimized:
+                pass
+        
     
     def predict(self, features):
         # sign(x.w+b)
         classification = np.sign(np.dot(np.array(features),self.w) + self.b)
+        
+        
+
+
+data_dict = {-1:np.array([[1,7],
+                          [2,8],
+                          [3,8],]),
+             
+             1:np.array([[5,1],
+                         [6,-1],
+                         [7,3],])}
